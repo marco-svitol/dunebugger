d26998de0b3153082695fec1efd341ce7c402e0f
marco <marco.cambon@gmail.com>
2023-12-10 23:19:57 +0100

    pwm

--- app/motor.py
+++ app/motor.py
@@ -3,8 +3,7 @@ import RPi.GPIO as GPIO
 from dunebuggerlogging import logger 
 
 def start(motornum, rotation="cw",speed=100):
-    pwm = GPIO.PWM(GPIOMap["Motor"+str(motornum)+"PWM"],100)
-    pwm.start(speed)
+    pwm = GPIO.PWM(GPIOMap["Motor"+str(motornum)+"PWM"],1000)
     logger.debug("motor "+str(motornum)+" start with rotation "+rotation+" and speed "+str(speed))
     if rotation == "cw":
         GPIO.output(GPIOMap["Motor"+str(motornum)+"In1"],GPIO.HIGH)
@@ -12,8 +11,11 @@ def start(motornum, rotation="cw",speed=100):
     else:
         GPIO.output(GPIOMap["Motor"+str(motornum)+"In1"],GPIO.LOW)
         GPIO.output(GPIOMap["Motor"+str(motornum)+"In2"],GPIO.HIGH)
+    pwm.start(100)
+    pwm.ChangeDutyCycle(speed)
 
 def stop(motornum):
     logger.debug("motor "+str(motornum)+" stopping")
     GPIO.output(GPIOMap["Motor"+str(motornum)+"In1"],GPIO.LOW)
     GPIO.output(GPIOMap["Motor"+str(motornum)+"In2"],GPIO.LOW)
+    GPIO.output(GPIOMap["Motor"+str(motornum)+"PWM"],GPIO.LOW)
