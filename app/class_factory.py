af6d08af48cb2e965939d591f5123a6afbd69d2d
Marco <62987024+marco-svitol@users.noreply.github.com>
2025-10-14 17:04:14 +0200

    NATS not blocking when disabled in config and start_listener awaited unconditionally. (#25)

--- app/class_factory.py
+++ app/class_factory.py
@@ -3,7 +3,7 @@ from terminal_interpreter import TerminalInterpreter
 from state_tracker import state_tracker
 from sequence import SequencesHandler
 from dunebugger_settings import settings
-from mqueue import NATSComm
+from mqueue import NATSComm, NullNATSComm
 from mqueue_handler import MessagingQueueHandler
 from gpio_handler import GPIOHandler, GPIO
 from audio_handler import AudioPlayer
@@ -18,13 +18,19 @@ command_interpreter = CommandInterpreter(mygpio_handler, sequence_handler)
 initialization_handler = InitializationHandler(command_interpreter)
 terminal_interpreter = TerminalInterpreter(command_interpreter)
 mqueue_handler = MessagingQueueHandler(sequence_handler, mygpio_handler, command_interpreter)
-mqueue = NATSComm(
-    nat_servers=settings.mQueueServers,
-    client_id=settings.mQueueClientID,
-    subject_root=settings.mQueueSubjectRoot,
-    mqueue_handler=mqueue_handler,
-)
 
-mqueue_handler.mqueue_sender = mqueue
+# Only create NATS connection if enabled
+if settings.mQueueEnabled:
+    mqueue = NATSComm(
+        nat_servers=settings.mQueueServers,
+        client_id=settings.mQueueClientID,
+        subject_root=settings.mQueueSubjectRoot,
+        mqueue_handler=mqueue_handler,
+    )
+    mqueue_handler.mqueue_sender = mqueue
+else:
+    mqueue = NullNATSComm()
+    # Don't set mqueue_sender for disabled NATS
+
 command_interpreter.mqueue_handler = mqueue_handler
 state_tracker.mqueue_handler = mqueue_handler
